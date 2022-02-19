f0e5c4c1b0161e32dec8e86bf86e26957722337e
andreev_e_v <Diablo372@yandex.ru>
2022-02-19 13:51:43 +0300

    homework lesson 4

---
+++ src/test/java/tests/GitHubTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubTest {
+    //1. Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+    //Понимаю что может, но затрудняюсь ответить почему. Завязка в том сколько дочерних div имеется у h1
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void selenideTestGit() {
+        open("https://github.com/");
+        //ввожу selenide в поиск
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        //проверяю содержит ли первый результат слово селенид и кликаю на него
+        $$("ul.repo-list li").first().$("a").shouldHave(Condition.text("selenide/")).click();
+        //кликаю по кнопке вики
+        $("#wiki-tab").click();
+        //проверяю есть ли страница Soft assertion и кликаю на нее
+        $("#wiki-body div ul li:nth-child(6) a").shouldHave(Condition.text("Soft assertions")).click();
+        //проверяю есть ли в тексте пример для Junit5
+        $("#wiki-body div ol:nth-child(16) li").shouldHave(Condition.text("Using JUnit5 extend test class"));
+
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //не понимаю почему не работает
+
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+
+        $("#column-b").shouldHave(Condition.text("A"));
+
+    }
+}
