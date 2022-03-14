70b6853c0b97f96b7550c589f414e6ea0a056bfa
jackagency <Diablo372@yandex.ru>
2022-03-14 19:57:56 +0300

    added remote_browser

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = System.getProperty("BROWSER", "chrome");
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("REMOTE_BROWSER");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
